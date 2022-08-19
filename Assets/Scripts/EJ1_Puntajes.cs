72b0faf43754038c215120afbb628cfc7d4c120f
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-08-19 10:42:57 -0300

    Ej2

---
+++ Assets/Scripts/EJ1_Puntajes.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ1_Puntajes : MonoBehaviour
+{
+    [SerializeField]int[] puntajes = new int[10]; //esto crea 10 fields para cargar ints
+
+
+
+    void Start()
+    {
+        Debug.Log("El mayor puntaje es " + ObtenerMayorPuntaje());
+        Debug.Log("El nivel con menor puntaje es " + ObtenerMayorPuntaje());
+        Debug.Log("El promedio es " + ObtenerPuntajePromedio());
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+        int mayorPuntaje = 0;
+
+        foreach (int puntaje in puntajes) //recorre cada puntaje
+        {
+            if (puntaje > mayorPuntaje)
+            {
+                mayorPuntaje = puntaje;
+            }
+            
+        }
+
+        return mayorPuntaje;
+    }
+
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int indiceNivelMenorPuntaje = 0;
+        int menorpuntaje = 0;
+        for(int i= 0;i<puntajes.Length;i++)
+        {
+            if (puntajes[i] < menorpuntaje )
+            {
+                menorpuntaje = puntajes[i];
+                indiceNivelMenorPuntaje = i;
+            }
+        }
+
+        return indiceNivelMenorPuntaje;
+    }
+    float ObtenerPuntajePromedio()
+    {
+        float promedio = 0;
+        int suma = 0;
+
+        foreach (int num in puntajes)
+        {
+            suma += num;
+        }
+        promedio = suma / puntajes.Length * 1.0f;
+
+
+        return promedio;
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    [SerializeField] string[] nombremeses = new string[12];
+    [SerializeField] int[] diasmensuales = new int[12];
+    public string fechaIngresada;
+
+    void Start()
+    {
+        ValidarFormatoFecha(fechaIngresada);
+    }
+    
+
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool fechaValida = false;
+
+
+        if (fecha.Length == 4)
+        {
+            string dia = ""; // agrega al final de la cadena 0
+            string mes = "";
+            dia += fecha[0];
+            dia += fecha[1];
+            int dianumber = int.Parse(dia); //lo pasa a int
+            mes += fecha[2];
+            mes += fecha[3];
+            int mesnumber = int.Parse(mes);
+            if (dianumber > 0 && dianumber <=31 && mesnumber>0 && mesnumber <=12)
+            {
+                fechaValida = true;
+            }
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+
+        return fechaValida;
+    }
+
+
+}
